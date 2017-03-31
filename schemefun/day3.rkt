2a8e8b4d9799556600819a6812fe0f19fcfe4317
Dave Musicant / thinkpad <dmusicant.thinkpad@carleton.edu>
2017-03-31 09:53:47 -0500

    Scripted auto-commit on change (2017-03-31 09:53:47) by gitwatch.sh

--- schemefun/day3.rkt
+++ schemefun/day3.rkt
@@ -1,9 +1,24 @@
+(define test
+  (lambda (a b)
+    (if (equal? a b)
+        'pass
+        'FAILED)))
+
 (define list-length
   (lambda (lst)
     (if (null? lst)
         0
         (+ 1 (list-length (cdr lst))))))
 
-(list-length '(a b c))
-(list-length '(a))
-(list-length ())
+;(test (list-length '(a b c)) 3)
+;(test (list-length '(a)) 1)
+;(test (list-length ()) 0)
+
+(require rackunit)
+
+(check-equal? (list-length '(a b c)) 3)
+(check-equal? (list-length '(a)) 1)
+(check-equal? (list-length ()) 0)
+
+
+;; Test if input is a list of numbers
