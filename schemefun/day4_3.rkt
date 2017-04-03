5e7d845bbfc9e520d95ac6a024a8375478fb237d
Dave Musicant / thinkpad <dmusicant.thinkpad@carleton.edu>
2017-04-03 10:53:44 -0500

    Scripted auto-commit on change (2017-04-03 10:53:44) by gitwatch.sh

--- schemefun/day4_3.rkt
+++ schemefun/day4_3.rkt
@@ -1,16 +1,23 @@
 (require rackunit)
 (define subst
   (lambda (old new input)
-    input))
+    (cond [(null? input) '()]
+          [(list? input)
+           (cons (subst old new (car input))
+                 (subst old new (cdr input)))]
+          [(equal? input old) new]
+          [else input])))
+    
 
 
 (check-equal? (subst 'b 'a '((b c) (b () d)))
                            '((a c) (a () d)))
+(subst 'b 'a '((b c) (b () d)))
 
 
 (define subsets
   (lambda (lst)
     lst))
 ;; not necessarily in this order
-(check-equal? (subsets '(1 2 3))
-              '( () (1) (2) (3) (1 2) (1 3) (2 3) (1 2 3)))
+;(check-equal? (subsets '(1 2 3))
+;              '( () (1) (2) (3) (1 2) (1 3) (2 3) (1 2 3)))
