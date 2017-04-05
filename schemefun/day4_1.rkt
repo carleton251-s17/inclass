26847118a245a4b018bc144a4a9c4ca096529fa5
Dave Musicant / thinkpad <dmusicant.thinkpad@carleton.edu>
2017-04-05 10:43:30 -0500

    Scripted auto-commit on change (2017-04-05 10:43:30) by gitwatch.sh

--- schemefun/day4_1.rkt
+++ schemefun/day4_1.rkt
@@ -11,4 +11,38 @@
     (lambda (b)
       (* a b))))
 
+;; uncurried
+(define insert-list
+  (lambda (item lst)
+    .....))
+
+(define curried-insert-list
+  (lambda (item)
+    (lambda (lst)
+        .... )))
+
+;; Show off how 'map' works in Scheme
+(define add-one
+  (lambda (x)
+    (+ x 1)))
+
+(add-one 12)
+
+;; do this for every item in a list
+;; map takes 2 parameters:
+;;   - a function of one parameter
+;;   - a list
+(map add-one '(1 4 2 9 6))
+
+;; "reduce" piece, in Scheme = foldl
+;; foldl takes 3 parameters
+;; - a function of two parameters
+;; - an initial value
+;; - a list
+(foldl cons '() '(1 2 3))
+(foldr cons '() '(1 2 3))
+
+;; create map (call it my-map)
+;; and my-foldl or my-foldr
+
 
