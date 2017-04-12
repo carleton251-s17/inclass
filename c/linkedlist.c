83ea637ec6fa11a71768758509b808e031687d29
Dave Musicant / thinkpad <dmusicant.thinkpad@carleton.edu>
2017-04-12 10:41:02 -0500

    Scripted auto-commit on change (2017-04-12 10:41:02) by gitwatch.sh

--- c/linkedlist.c
+++ c/linkedlist.c
@@ -5,8 +5,17 @@ struct LinkedList {
    struct LinkedList *next;
 };
 
+LinkedList *insertFrontLL(LinkedList *list, int item) {
+   LinkedList *cell = malloc(sizeof(LinkedList));
+   (*cell).value = item;
+   (*cell).next = list;
+   return cell;
+}
+
+
 typedef   struct LinkedList   LinkedList;
 
 int main() {
    LinkedList *list = NULL;
+   list = insertFrontLL(list, 3);
 }
