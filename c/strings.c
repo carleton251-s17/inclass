9bc110586e172bf91fdbded82ef5cb40cee28172
Dave Musicant / thinkpad <dmusicant.thinkpad@carleton.edu>
2017-04-17 10:59:08 -0500

    Scripted auto-commit on change (2017-04-17 10:59:08) by gitwatch.sh

--- c/strings.c
+++ c/strings.c
@@ -23,7 +23,7 @@ int main() {
     printf("%s\n",together);
 
     // Just for Jack
-    char *jack = "jack\0";
+    char *jack = "jack";
     char *again = "again";
     printf("%p\n",jack);
     printf("%p\n",again);
