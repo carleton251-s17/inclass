b1d8b2f23d711c0458b79a22856f1c4e6412326e
Dave Musicant / thinkpad <dmusicant.thinkpad@carleton.edu>
2017-04-17 10:43:16 -0500

    Scripted auto-commit on change (2017-04-17 10:43:16) by gitwatch.sh

--- c/strings.c
+++ c/strings.c
@@ -7,12 +7,13 @@ int main() {
     //word1[5] = 'q';
     printf("%s\n", word1);
 
-    /* char *word2 = "world"; */
-    /* word2[0] = 'b'; */
-    /* printf("%s\n", word2); */
+    //char *word3 = malloc(6*sizeof(char));
+    char *word3 = calloc(6,sizeof(char));
+    strcpy(word3,"weitz");
+    printf("%s\n", word3);
 
-    /* //char *word3 = malloc(6*sizeof(char)); */
-    /* char *word3 = calloc(6,sizeof(char)); */
-    /* strcpy(word3,"weitz"); */
-    /* printf("%s\n", word3); */
+    /*char *word2 = "world";
+    word2[0] = 'b';
+    printf("%s\n", word2);*/
+
 }
