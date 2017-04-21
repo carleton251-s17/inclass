6790407eecf235e0474adf8c9e7446824663d326
Dave Musicant / thinkpad <dmusicant.thinkpad@carleton.edu>
2017-04-21 10:18:59 -0500

    Scripted auto-commit on change (2017-04-21 10:18:59) by gitwatch.sh

---
+++ c/fptr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int add(int x, int y) {
+  return x + y;
+}
+
+int multiply(int x, int y) {
+  return x*y;
+}
+
+int doit(int (*f)(int,int)  ,  int x, int y) {
+  return (*f)(x,y);
+}
+
+int main() {
+  printf("%i\n", doit(add,3,5));
+  printf("%i\n", doit(multiply,3,5));
+}
