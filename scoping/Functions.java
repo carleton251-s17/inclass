c4cf08c32fd27f500a3c187013f32aaf18eca5f8
Dave Musicant / thinkpad <dmusicant.thinkpad@carleton.edu>
2017-05-12 09:47:52 -0500

    Scripted auto-commit on change (2017-05-12 09:47:52) by gitwatch.sh

--- scoping/Functions.java
+++ scoping/Functions.java
@@ -1,7 +1,11 @@
+interface Function {
+   public int f(int x, int y);
+}
+
 public class Functions {
 
-   public static void evaluate(Function f, int x, int y) {
-      System.out.println(f(x,y));
+   public static void evaluate(Function fobj, int x, int y) {
+      System.out.println(fobj.f(x,y));
    }
 
    public static void main(String[] args) {
