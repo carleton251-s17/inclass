a2131b9c91a49f2a70f5f414a60cc54e8cef0e41
Dave Musicant / thinkpad <dmusicant.thinkpad@carleton.edu>
2017-05-12 09:46:07 -0500

    Scripted auto-commit on change (2017-05-12 09:46:07) by gitwatch.sh

---
+++ scoping/Functions.java
@@ -0,0 +1,12 @@
+public class Functions {
+
+   public static void evaluate(Function f, int x, int y) {
+      System.out.println(f(x,y));
+   }
+
+   public static void main(String[] args) {
+
+      evaluate(add, 5 3);
+
+   }
+}
