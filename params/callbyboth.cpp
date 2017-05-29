ff91f869245b8645ea9597375fca102d8d4d9626
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2017-05-29 10:45:08 -0500

    Scripted auto-commit on change (2017-05-29 10:45:08) by gitwatch.sh

--- params/callbyboth.cpp
+++ params/callbyboth.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void foo(int x) {
+void foo(int x, int call-by-ref y) {
    x = 6;
+   y = 7;
 }
 
 int main() {
    int a = 5;
-   printf("%i\n",a);
+   int b = 4;
    foo(a);
-   printf("%i\n",a);
+   printf("%i %i\n",a,b);
 }
